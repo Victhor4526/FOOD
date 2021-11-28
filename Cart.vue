ab20ac414ee5bfb4bd819327548e417c8e8740c7
Victhor4526 <75048758+Victhor4526@users.noreply.github.com>
2021-11-28 17:42:19 +0800

    Add files via upload

---
+++ Cart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1> Cart</h1>
+    <div class="products">
+      <div v-for="(product, index) in cart" :key="index">
+        <h3>{{product.name}}</h3>
+        <img :src="product.image" />
+        <div>{{product.cost}}</div>
+        <button v-on:click="removeItemFromCart(product)">Remove Cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["cart"],
+  methods: {
+    removeItemFromCart(product) {
+      this.$emit("removeItemFromCart", product);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
